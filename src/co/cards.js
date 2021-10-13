5b99961e3802318139d2011e3d176e4f2a59c6b5
Lalit Gurjar <83588799+Lalit3016@users.noreply.github.com>
2021-10-13 09:55:14 +0530

    create react app

--- src/co/cards.js
+++ src/co/cards.js
@@ -1,1 +1,31 @@
+import React, { useReducer } from 'react'
 
+const Users=({ loading, users })=>{
+    const mainstyle={
+        display:'grid',
+        gridTemplateColumns: 'repeat('+3+','+1+' fr)',
+    };
+    return loading?(
+        <div id="main" align="center">
+            <img src="https://upload.wikipedia/commons/b/b1/Loading_icon.gif" alt="Loading.." className="loader" />
+        </div>
+    ):
+    (
+        <div id="main" style={mainstyle}>
+            {
+                users.map(user=>
+                <div className="profile_cont" key={user.id} >
+                    {
+                        console.log(user)
+                    }   
+                    <img src={user.avatar} alt={user.avatar} className="avatar"></img>
+                    <h1 className="name">{user.first_name} {user.last_name}</h1> 
+                    <p className="email">{user.email}</p>
+                    <p>User ID: {user.id}</p>
+                </div>    
+               )
+            }
+        </div>
+    )
+}
+export default Users;
